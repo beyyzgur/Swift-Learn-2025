beab1732c59e0dbbf132d73470de4c14cefc3094
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-05-13 16:01:48 +0300

    update: BirthdayNoteTaker for basic UserDefault example

--- BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController { // commit atmayı UNUTMAAA!!!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName, let newBirthday = storedBirthday as? String {
+            nameLabel.text = "Name: \(newName)"
+            birthdayLabel.text  = "Birthday: \(newBirthday)"
+        }
     }
-
-
+    
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        //UserDefaults.standard.synchronize()
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text  = "Birthday: \(birthdayTextField.text!)"
+    }
 }
-
