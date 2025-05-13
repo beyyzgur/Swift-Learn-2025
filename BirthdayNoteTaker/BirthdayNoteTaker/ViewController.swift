eefaed5ea056b85ec833d61f51beaa077a5e8f10
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-05-13 20:56:24 +0300

    feat: add delete feature for UserDefault data

--- BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController { // commit atmayı UNUTMAAA!!!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var birthdayLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,9 +29,22 @@ class ViewController: UIViewController { // commit atmayı UNUTMAAA!!!
     @IBAction func saveButtonClicked(_ sender: UIButton) {
         UserDefaults.standard.set(nameTextField.text, forKey: "name")
         UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
-        //UserDefaults.standard.synchronize()
+//        UserDefaults.standard.synchronize()
         
         nameLabel.text = "Name: \(nameTextField.text!)"
         birthdayLabel.text  = "Birthday: \(birthdayTextField.text!)"
+    }
+    
+    @IBAction func deleteButtonClicked(_ sender: UIButton) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName as? String) != nil && (storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name: "
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text  = "Birthday: "
+            print("silindi")
+        }
     }
 }
