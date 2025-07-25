f7f073c94e8b92887b9593da9e3e9881676bad05
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-07-25 13:15:19 +0300

    feat: add CustomKeyboardApp to learn how to make custom keyboards

---
+++ CustomKeyboard/CustomKeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomKeyboard
+//
+//  Created by beyyzgur on 25.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
