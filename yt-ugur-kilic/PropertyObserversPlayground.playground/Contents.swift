e3ef5ee30bf8c80e951371593e4da06e8cfb383b
Ayşe Beyza Gür <beyyzgur@gmail.com>
2025-11-09 23:59:24 +0300

    feat: practise property observers

---
+++ yt-ugur-kilic/PropertyObserversPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+struct Person {
+    var fullName: String {
+        didSet {
+            nameChanged()
+        }
+        willSet {
+            print("fullName is changed from \(fullName) to \(newValue)")
+        }
+    }
+    var age: Int {
+        didSet {
+            print("new age = \(age)")
+            ageChanged()
+        }
+    }
+    private var birthYear: Int = 0
+    
+    init(fullName: String, age: Int) {
+        self.fullName = fullName
+        self.age = age
+    }
+    
+    // MARK: - Private Methods
+    
+    private func nameChanged() {
+        print("name has changed")
+    }
+    
+    private mutating func ageChanged() {
+        birthYear = 2025 - age
+        print(birthYear)
+    }
+}
+
+var person = Person(fullName: "beyza", age: -1)
+person.age = 0
+person.fullName = "beyza gur"
